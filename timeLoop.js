ca52eb877d5c4438aae1a5117f334dad96e3c4c5
Peter Suchsland <visiondoctor2020@gmail.com>
2022-08-15 15:11:10 +0700

    beautified with crlfs removed; also bug date bug fixed.

--- timeLoop.js
+++ timeLoop.js
@@ -1,30 +1,30 @@
 let tzMap = {
-  "PHX":"America/Phoenix",
-  "JFK":"America/New_York",
-  "PNH":"Asia/Phnom_Penh",
-  "LAX":"America/Los_Angeles",
-  "DEN":"America/Denver",
-  "NRT":"Asia/Tokyo",
-  "MEL":"Australia/Melbourne",
-  "ORD":"America/Chicago",
-  "FRA":"Europe/Berlin",
-  "AKL":"Pacific/Auckland",
-  "AMS":"Europe/Amsterdam",
-  "LHR":"Europe/London",
-  "PER":"Australia/Perth",
-  "PVG":"Asia/Shanghai",
-  "HKT":"Asia/Hong_Kong",
-  "TLV":"Asia/Tel_Aviv",
-  "MAA":"Asia/Kolkata",
-  "GRU":"America/Sao_Paulo",
-  "DME":"Europe/Moscow",
-  "HNL":"Pacific/Honolulu",
-  "ICN":"Asia/Seoul",
+  "PHX": "America/Phoenix",
+  "JFK": "America/New_York",
+  "PNH": "Asia/Phnom_Penh",
+  "LAX": "America/Los_Angeles",
+  "DEN": "America/Denver",
+  "NRT": "Asia/Tokyo",
+  "MEL": "Australia/Melbourne",
+  "ORD": "America/Chicago",
+  "FRA": "Europe/Berlin",
+  "AKL": "Pacific/Auckland",
+  "AMS": "Europe/Amsterdam",
+  "LHR": "Europe/London",
+  "PER": "Australia/Perth",
+  "PVG": "Asia/Shanghai",
+  "HKT": "Asia/Hong_Kong",
+  "TLV": "Asia/Tel_Aviv",
+  "MAA": "Asia/Kolkata",
+  "GRU": "America/Sao_Paulo",
+  "DME": "Europe/Moscow",
+  "HNL": "Pacific/Honolulu",
+  "ICN": "Asia/Seoul",
 };
 
 
-anyNumberSuffix = function (n) {
-  if (typeof (n) === 'string' || n < 0) {
+anyNumberSuffix = function(n) {
+  if (typeof(n) === 'string' || n < 0) {
     return n;
   }
   let suffix = '';
@@ -40,12 +40,11 @@ anyNumberSuffix = function (n) {
       lastDigit = last2Digits.groups.lastDigit;
     }
   } catch (err) {
-  // console.log("Error: "+ err);
+    // console.log("Error: "+ err);
   }
   if (secondtoLast === '1') {
     suffix = 'th';
-  }
-  else {
+  } else {
     switch (lastDigit) {
       case undefined:
         suffix = '';
@@ -68,31 +67,38 @@ anyNumberSuffix = function (n) {
 };
 
 
-mkLongOptions = function(tz=string, hr12=boolean) {
+mkLongOptions = function(tz = string, hr12 = boolean) {
   // return map of options that include timeZone and hour12
-  let options = {weekday:'long', year: 'numeric',month: 'long',day: 'numeric', hour: 'numeric',minute: 'numeric',second: 'numeric'};
+  let options = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric'
+  };
   options.timeZone = tz;
   options.hours12 = hr12;
   return options;
-}
+};
 
 
 function changeTime() {
-  let t=setInterval("getFullDateAndTime();", 1000); // every second
-}
+  let t = setInterval("getFullDateAndTime();", 1000); // every second
+};
 
 
 function getFullDateAndTime() {
-  //let pat =  ;
-  let pat = /\b\d\b/ ;
+  let pat = /\b\d\d\b/;
   let dnow = new Date();
-  for (airportCode in tzMap ) {
+  for (airportCode in tzMap) {
     dateTimeFormat = new Intl.DateTimeFormat('en-US', mkLongOptions(tzMap[airportCode], true));
     let d = dateTimeFormat.format(dnow); // apply formatting using options
     let day = Number(d.match(pat));
-    d = d.replace(pat,anyNumberSuffix(day)); // turn 18 into 18th
-    document.getElementById(airportCode).innerHTML =  airportCode + ":  "+ d;
+    d = d.replace(pat, anyNumberSuffix(day)); // turn 18 into 18th
+    document.getElementById(airportCode).innerHTML = airportCode + ":  " + d;
   };
-}
+};
 
 changeTime();
