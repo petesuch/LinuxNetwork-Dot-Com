286f7d3620a847b3ad182831eb74fe1c345d492c
Peter Suchsland <visiondoctor2020@gmail.com>
2026-08-22 15:45:42 +0700

    Update time zones for various cities

--- timezoneTable.js
+++ timezoneTable.js
@@ -7,7 +7,7 @@ function timedMsg() {
 function change_time() {
     var d1 = new Date().toLocaleString("en-US", {timeZone: "America/Phoenix"})
     var d2 = new Date().toLocaleString("en-US", {timeZone: "America/New_York"})
-    var d3 = new Date().toLocaleString("en-US", {timeZone: "Asia/Phnom_Penh"}) // Phnom Penh now served by Techo Int'l Airport (KTI)
+    var d3 = new Date().toLocaleString("en-US", {timeZone: "Asia/Phnom_Penh"})
     var d4 = new Date().toLocaleString("en-US", {timeZone: "America/Los_Angeles"})
 
     var d5 = new Date().toLocaleString("en-US", {timeZone: "America/Denver"})
@@ -28,7 +28,7 @@ function change_time() {
     var d13 = new Date().toLocaleString("en-US", {timeZone: "Australia/Perth"})
     var d14 = new Date().toLocaleString("en-US", {timeZone: "Asia/Shanghai"})
     var d15 = new Date().toLocaleString("en-US", {timeZone: "Asia/Hong_Kong"})
-    var d16 = new Date().toLocaleString("en-US", {timeZone: "Asia/Tel_Aviv"})
+    var d16 = new Date().toLocaleString("en-US", {timeZone: "Asia/Tehran"})
 
     var d17 = new Date().toLocaleString("en-US", {timeZone: "Asia/Kolkata"})
     var d18 = new Date().toLocaleString("en-US", {timeZone: "America/Sao_Paulo"})
@@ -36,25 +36,25 @@ function change_time() {
     var d20 = new Date().toLocaleString("en-US", {timeZone: "Pacific/Honolulu"})
 
 
-    document.getElementById("phx_time").innerHTML = '<a href="https://www.timeanddate.com/weather/usa/phoenix/ext" target="_blank">Tucson: ' +d1+ '</a>';
+    document.getElementById("phx_time").innerHTML = "Phoenix: " + d1 ;
     document.getElementById("jfk_time").innerHTML = "New York: " + d2;
-    document.getElementById("kti_time").innerHTML = "Phnom Penh (KTI): "+ d3;
-    document.getElementById("lax_time").innerHTML = "San Francisco: "+ d4;
+    document.getElementById("kti_time").innerHTML = "Phnom Penh: " + d3;
+    document.getElementById("lax_time").innerHTML = "San Francisco: " + d4;
 
-    document.getElementById("den_time").innerHTML = "Boulder: " +d5;
+    document.getElementById("den_time").innerHTML = "Denver: " + d5;
     document.getElementById("nrt_time").innerHTML = "Tokyo: " + d6;
-    document.getElementById("mel_time").innerHTML = "Melbourne: "+ d7;
-    document.getElementById("ord_time").innerHTML = "Chicago: "+ d8;
+    document.getElementById("mel_time").innerHTML = "Melbourne: " + d7;
+    document.getElementById("ord_time").innerHTML = "Chicago: " + d8;
 
-    document.getElementById("fra_time").innerHTML = "Frankfurt: "+ d9;
+    document.getElementById("fra_time").innerHTML = "Frankfurt: " + d9;
     document.getElementById("akl_time").innerHTML = "Auckland: " + d10;
-    document.getElementById("ams_time").innerHTML = "Amsterdam: "+ d11;
-    document.getElementById("lhr_time").innerHTML = "London: "+ d12;
+    document.getElementById("ams_time").innerHTML = "Amsterdam: " + d11;
+    document.getElementById("lhr_time").innerHTML = "London: " + d12;
 
     document.getElementById("per_time").innerHTML = "Perth: "+ d13;
     document.getElementById("pvg_time").innerHTML = "Shanghai: " + d14;
     document.getElementById("hkt_time").innerHTML = "Hong Kong: "+ d15;
-    document.getElementById("tlv_time").innerHTML = "Tel Aviv: "+ d16;
+    document.getElementById("tlv_time").innerHTML = "Tehran: "+ d16;
 
     document.getElementById("maa_time").innerHTML = "Chennai: "+ d17;
     document.getElementById("gru_time").innerHTML = "Sao Paulo: " + d18;
